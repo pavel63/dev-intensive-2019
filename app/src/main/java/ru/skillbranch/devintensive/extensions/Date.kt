bc534f54d43771ffce488d1956429ac1e546e2be
Pavel <Pavel>
2019-07-05 00:27:38 +0300

    make humanize diff for negate values

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -40,38 +40,77 @@ enum class TimeUnits {
 
 
 fun Date.humanizeDiff(): String {
-    val timeDiff = Date().time -this.time
+    var timeDiff = Date().time -this.time
 
-    return when (timeDiff) {
-        in 0..999 -> "только что"
+    if(timeDiff>0) {
 
-        in 1000..44_999 -> "несколько секунд назад"
+        return when (timeDiff) {
+            in 0..999 -> "только что"
 
-        in 45_000..75_000 -> "минуту назад"
+            in 1000..44_999 -> "несколько секунд назад"
 
-        in 75_000..2_699_999 -> {
-            val strNumb = timeDiff / 60_000
-            "$strNumb ${(Utils .numbersDeclination(strNumb.toString(), "minutes"))} назад"
+            in 45_000..75_000 -> "минуту назад"
+
+            in 75_000..2_699_999 -> {
+                val strNumb = timeDiff / 60_000
+                "$strNumb ${(Utils.numbersDeclination(strNumb.toString(), "minutes"))} назад"
+            }
+
+            in 2_700_000..4_499_999 -> "час назад"
+
+            in 4_500_000..79_199_999 -> {
+                val strNumb = timeDiff / 3_600_000
+                "$strNumb ${(Utils.numbersDeclination(strNumb.toString(), "hours"))} назад"
+            }
+
+            in 79_200_000..92_599_999 -> "день назад"
+
+            in 92_600_000..31_103_999_999 -> {
+                val strNumb = timeDiff / 86_000_000
+                "$strNumb ${(Utils.numbersDeclination(strNumb.toString(), "days"))} назад"
+            }
+
+            in 31_104_000_000..Long.MAX_VALUE -> "более года назад"
+
+            else -> ""
         }
 
-        in 2_700_000..4_499_999 -> "час назад"
+    } else {
 
-        in 4_500_000..79_199_999 -> {
-            val strNumb = timeDiff / 3_600_000
-            "$strNumb ${(Utils .numbersDeclination(strNumb.toString(), "hours"))} назад"
+      timeDiff*=-1
+
+        return when (timeDiff) {
+            in 0..999 -> "только что"
+
+            in 1000..44_999 -> "через несколько секунд"
+
+            in 45_000..75_000 -> "через минуту"
+
+            in 75_000..2_699_999 -> {
+                val strNumb = timeDiff / 60_000
+                "через $strNumb ${(Utils.numbersDeclination(strNumb.toString(), "minutes"))}"
+            }
+
+            in 2_700_000..4_499_999 -> "через час"
+
+            in 4_500_000..79_199_999 -> {
+                val strNumb = timeDiff / 3_600_000
+                "через $strNumb ${(Utils.numbersDeclination(strNumb.toString(), "hours"))}"
+            }
+
+            in 79_200_000..92_599_999 -> "через день"
+
+            in 92_600_000..31_103_999_999 -> {
+                val strNumb = timeDiff / 86_000_000
+                "через $strNumb ${(Utils.numbersDeclination(strNumb.toString(), "days"))}"
+            }
+
+             in 31_104_000_000..Long.MAX_VALUE -> "более чем через год"
+
+            else -> ""
         }
-
-        in 79_200_000..92_599_999 -> "день назад"
-
-        in 592_600_000..31_103_999_999 -> {
-            val strNumb = timeDiff / 86_000_000
-            "$strNumb ${(Utils .numbersDeclination(strNumb.toString(), "days"))} назад"
-        }
-
-        in 31_104_000_000..Long.MAX_VALUE -> "более года назад"
-
-        else -> ""
     }
+
 
 }
 
