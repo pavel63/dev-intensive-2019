1c7d09af10b01b709981054d7252c66fb1d91a8c
Pavel <Pavel>
2019-07-22 09:26:52 +0300

    inter commit

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,97 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
+import ru.skillbranch.devintensive.models.Bender
+
+
+fun EditText .onClickKeyboardDoneButton(funExecute: () -> Unit) {
+    this.setOnEditorActionListener { _, actionId, _ ->
+        when (actionId) {
+            EditorInfo.IME_ACTION_DONE -> {
+                funExecute.invoke()
+                true
+            }
+            else -> false
+        }
+    }
+}
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var status : String ?= null
+    private var benderColor : Triple<Int,Int,Int> ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       textHeader.text = Bender().askQuestion()
+
+        et_message?.onClickKeyboardDoneButton { myFunctionToExecuteWhenUserClickDone() }
+
     }
-}
+
+
+
+
+   private fun myFunctionToExecuteWhenUserClickDone() {
+
+       this.hideKeyboard()
+
+        val (first, second) = Bender().listenAnswer(et_message ?.text.toString())
+
+       status = first
+       benderColor = second
+
+      populateWiews(status ,benderColor)
+
+    }
+
+
+
+
+    private fun populateWiews(innerStatus : String?, innerColor: Triple<Int,Int,Int>?){
+        textHeader ?.text = innerStatus
+        benderImage ?.setColorFilter(Color.rgb(innerColor?.first ?:256, innerColor?.second?:256 ,innerColor?.third?:256), PorterDuff.Mode.MULTIPLY)
+    }
+
+
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState .putString("STATUS" ,status)
+       // outState .putString("QUESTION")
+        outState .putSerializable("COLOR", benderColor)
+    }
+
+
+
+
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        status = savedInstanceState .getString("STATUS" ,status)
+        benderColor = savedInstanceState .getSerializable("COLOR")as? Triple<Int, Int, Int>
+
+       populateWiews(status ,benderColor)
+
+    }
+
+
+    }
+
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,49 @@
+package ru.skillbranch.devintensive.models
+
+class Bender (var status : Status=Status.NORMAL, var question :Question = Question.NAME){
+
+
+    fun askQuestion() : String = when(question){
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+
+
+
+    fun listenAnswer(answer:String):Pair<String, Triple<Int,Int,Int>>{
+        return if (question.answers.contains(answer)){
+            "Отлично - это правильный ответ!" to status.color
+        }else{
+            "Это не правильный ответ" to Status.WARNING.color
+        }
+    }
+
+
+
+
+    enum class Status(val color: Triple<Int,Int,Int>) {
+        NORMAL(Triple(255, 255, 255)) ,
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0)) ;
+    }
+
+
+
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")),
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
+        BDAY("Когда меня создали?", listOf("2993")),
+        SERIAL("Мой серийный номер?", listOf("2716057")),
+        IDLE("На этом все, вопросов больше нет", listOf())
+    }
+
+
+}
